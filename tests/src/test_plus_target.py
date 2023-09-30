67dd1b2d795756299f3d11c27c428f4b46c10962
aki2274 <s2110307@s.tsukuba.ac.jp>
2023-09-30 13:34:14 +0900

    strandplusの単体テストを追加

---
+++ tests/src/test_plus_target.py
@@ -0,0 +1,25 @@
+import pytest
+from scr import plus_target
+
+def test_get_CtoT_target():
+    test_seq_list = ['CCANNNNNNNNNNNNNNNNNNTGG','CAGNNNNNNNNNNNNNNNNGG','CAGNNNNNNNNNNNNNNNNGGCGANNNNNNNNNNNNNNNNGG',
+                       'CAGCAANNNNNNNNNNNNNGGTGG']
+    result = []
+    for i in range(len(test_seq_list)):
+        seq = test_seq_list[i]
+        target = plus_target.get_CtoT_target(seq)
+        result.append(plus_target.CtoT_target_start(target,seq))
+    expected = [[],[0],[0,21],[0,3]]
+    assert result == expected
+
+
+def test_get_AtoG_target():#listの0要素目はマッチするべきだが、配列の一番最後である場合なので検索できなくてもOK？
+    test_seq_list = ['CCANNNNNNNNNNNNNNTGG','CCANNNNNNNNNNNNNNTGGNNNN','CCANNNNNNNNNNNNNNNNNTGGCCANNNNNNNNNNNNNNNNNTGG',
+                       'CCNCCCNNNNNNNNNNNNNNTGGTGGNNNN']
+    result = []
+    for i in range(len(test_seq_list)):
+        seq = test_seq_list[i]
+        target = plus_target.get_AtoG_target(seq)
+        result.append(plus_target.AtoG_target_start(target,seq))
+    expected = [[],[17],[20,43],[20,23]]
+    assert result == expected
