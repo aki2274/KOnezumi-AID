9d17b892e93a6bc5fb61db64ae659450f686e432
aki2274 <s2110307@s.tsukuba.ac.jp>
2023-10-16 23:37:00 +0900

    Update get_guiderna_seq.py

--- src/get_guiderna_seq.py
+++ src/get_guiderna_seq.py
@@ -1,7 +1,7 @@
 import re
 from typing import Tuple
-import numpy as np
-import pandas as pd
+#import numpy as np
+#import pandas as pd
 
 def get_re_comp(seq:str)->str:
     complementary = {'A':'T','T':'A','G':'C','C':'G'}
