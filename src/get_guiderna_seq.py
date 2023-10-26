3a8f2089ccdc07197c79e3c877c880d733bbd779
Akihiro Kuno <akikuno@github.com>
2023-10-27 06:17:34 +0900

    refactoring `get_ct_target_num` + 関数名をget_ct_target_numからget_index_of_ct_target_seqとしました + 引数名の`que`を`seq`としました + for文の`i`は数字を示唆するのですが、今回は数字ではないので`target`としました + 可読性を優先するために、リスト内包表記ではなくfor文を使い、`num[0]`といった部分をなくしました + 不要と思われる`list(dict.fromkeys(result))`をなくしました

--- src/get_guiderna_seq.py
+++ src/get_guiderna_seq.py
@@ -7,6 +7,7 @@ import pandas as pd
 
 complementary = {"A": "T", "T": "A", "G": "C", "C": "G", "N": "N"}
 
+
 def get_revcomp(seq: str) -> str:
     return "".join(complementary[base.upper()] for base in seq[::-1])
 
@@ -14,25 +15,28 @@ def get_revcomp(seq: str) -> str:
 def find_ct_target_seq(seq: str) -> list[str]:
     matches = re.findall(r"(?=(\w{21}GG))", seq)
     # Check for the presence of "CAA", "CAG", or "CGA" in the next 3 positions.
-    targets = [
-        match for match in matches
-        if any(match[i:i+3] in {"CAA", "CAG", "CGA"} for i in range(1, 4))
-    ]
+    targets = [match for match in matches if any(match[i : i + 3] in {"CAA", "CAG", "CGA"} for i in range(1, 4))]
     # Remove duplicates while preserving order
     return list(dict.fromkeys(targets))
 
 
-def get_ct_target_num(que, target_list) -> list:  # CXXのCの位置を出す
-    result = []
-    for i in target_list:
-        matches = re.finditer(i, que)
-        num = [
-            re.search(r"(CAA|CAG|CGA)", str(match.group())).start() + match.start()
-            for match in matches
-        ]
-        result.append(num[0])
-    result = list(dict.fromkeys(result))
-    return result
+def get_index_of_ct_target_seq(seq: str, targets: list[str]) -> list[int]:
+    positions = []
+    for target in targets:
+        for match in re.finditer(target, seq):
+            position = re.search(r"(CAA|CAG|CGA)", match.group()).start() + match.start()
+            positions.append(position)
+    return positions
+
+
+# def get_ct_target_num(que, target_list) -> list:  # CXXのCの位置を出す
+#     result = []
+#     for i in target_list:
+#         matches = re.finditer(i, que)
+#         num = [re.search(r"(CAA|CAG|CGA)", str(match.group())).start() + match.start() for match in matches]
+#         result.append(num[0])
+#     result = list(dict.fromkeys(result))
+#     return result
 
 
 def find_ag_target(seq: str) -> list:
@@ -40,10 +44,7 @@ def find_ag_target(seq: str) -> list:
     target_list = []
     target_list = [
         True
-        if target[20:23] == "TGG"
-        or target[19:22] == "TGG"
-        or target[18:21] == "TGG"
-        or target[17:20] == "TGG"
+        if target[20:23] == "TGG" or target[19:22] == "TGG" or target[18:21] == "TGG" or target[17:20] == "TGG"
         else False
         for target in match
     ]
