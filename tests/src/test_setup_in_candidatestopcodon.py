061d494c5f4b1242c922e6cf3e73485bfa08d8fb
aki2274 <s2110307@s.tsukuba.ac.jp>
2023-11-03 12:03:46 +0900

    make a test 'setup' function.

--- tests/src/test_setup_in_candidatestopcodon.py
+++ tests/src/test_setup_in_candidatestopcodon.py
@@ -4,19 +4,21 @@ from src.get_candidate_stopcodon_index import setup
 
 # Make test data
 # txStart とexonStart[0]は一致している必要がある
-
+# test_df = pd.read_csv("test/data/test_genedata.csv")
 gene_data = [
-    "A",
-    "t1",
-    "1",
-    "+",
-    "0",
-    "30",
-    "10",
-    "25",
-    "3",
-    "0,5,15",
-    "3,13,29",
+    [
+        "A",
+        "t1",
+        "1",
+        "+",
+        "0",
+        "30",
+        "10",
+        "25",
+        "3",
+        "0,5,15",
+        "3,13,29",
+    ]
 ]
 
 col_names = [
@@ -34,16 +36,19 @@ col_names = [
 ]
 test_df = pd.DataFrame(gene_data, columns=col_names)
 
-test_name = ["t1"]
+test_name = ["t1"]  # test_df["name"]
 
-test_seq = {"t1::1:0-100": "NNNNNNNNNNATGTNNNNNNNNNNNNNNNN"}
+test_seq = [
+    {"t1::1:0-100": "NNNNNNNNNNATGTNNNNNNNNNNNNNNNN"}
+]  # dict key is "{transcripts_name}::{chrom}:{txStart}-{txEnd}". Value is orf_seq.
 
+# setup module return DataClass
 expected = [
     {
         "orf_seq": "NNNNNNNNNNATGTNNNNNNNNNNNNNNNN",
         "data": test_df[test_df["name"] == "t1"],
         "txStart": 0,
-        "txend": 30,
+        "txEnd": 30,
         "cdsStart": 10,
         "cdsEnd": 25,
         "exonCount": 3,
@@ -55,7 +60,7 @@ expected = [
 
 @pytest.mark.parametrize(
     "transcripts_name,gene_df,gene_seq_data,expected",
-    zip(test_name, test_df, test_seq, expected),
+    zip(test_name, [test_df], test_seq, expected),
 )
-def test_get_exon_seq(transcripts_name, gene_df, gene_seq_data, expected):
+def test_setup_data(transcripts_name, gene_df, gene_seq_data, expected):
     assert setup(transcripts_name, gene_df, gene_seq_data) == expected
