3fbc586bba6f8c7ee0c156252a929f6151083d6f
aki2274 <s2110307@s.tsukuba.ac.jp>
2023-11-16 00:26:55 +0900

    Update test_change_candidate_stopcodonindex.py

--- tests/src/test_change_candidate_stopcodonindex.py
+++ tests/src/test_change_candidate_stopcodonindex.py
@@ -3,7 +3,7 @@ import os
 import pytest
 from src.set_gene_dataclass import set_dataclass
 from src.import_dataset import read_csv
-from src.get_candidate_stopcodon_index import get_startcodon_exonindex
+from src.get_candidate_stopcodon_index import get_startcodon_exon_num
 from src.change_exon_index_to_gene import (
     get_range_of_exon,
     get_candidate_codon_exon_index,
@@ -17,7 +17,7 @@ test_df = read_csv(test_data_path)
 test_seq = {"t1::chr1:0-30": "NNNNNNNNNNATGTNANNNNNNNNNNNNNN"}
 test_gene_dataclass = set_dataclass("t1", test_df, test_seq)
 
-test_candidate_codon_exon_index = [get_startcodon_exonindex(test_gene_dataclass)]
+test_candidate_codon_exon_index = [get_startcodon_exon_num(test_gene_dataclass)]
 
 expected = [[[0, 8], [8, 22]]]
 
