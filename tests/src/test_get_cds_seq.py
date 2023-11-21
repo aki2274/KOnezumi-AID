41461368d041653b96619d4aa4b7e0b499ae38c8
aki2274 <s2110307@s.tsukuba.ac.jp>
2023-11-21 10:49:05 +0900

    Update test_get_cds_seq.py
    エクソンの始まりを明示したテストケースを追加しました。

--- tests/src/test_get_cds_seq.py
+++ tests/src/test_get_cds_seq.py
@@ -3,7 +3,6 @@ import os
 import pytest
 import numpy as np
 from dataclasses import dataclass
-from src.import_dataset import read_pkl
 from src.set_gene_dataclass import set_dataclass
 from src.get_candidate_stopcodon_index import (
     get_exon_seq,
@@ -15,64 +14,136 @@ from src.get_candidate_stopcodon_index import (
 #####
 # test dataset
 #####
-test_data_path = os.path.join(
-    os.path.dirname(__file__), "..", "data", "test_genedata.pkl"
-)
-test_list_dict = read_pkl(test_data_path)
-orf_seq_dict = {"t1::chr1:0-30": "NNNNNNNNNNATGTNANNNNNNNNNNNNNN"}
-test_gene_dataclass = set_dataclass("t1", test_list_dict, orf_seq_dict)
+input_genedata = [
+    {
+        "geneName": "A",
+        "name": "t1",
+        "chrom": "chr1",
+        "strand": "+",
+        "txStart": "0",
+        "txEnd": "100",
+        "cdsStart": "10",
+        "cdsEnd": "90",
+        "exonCount": "3",
+        "exonStarts": "0,25,60",
+        "exonEnds": "15,50,95",
+    },
+    {
+        "geneName": "B",
+        "name": "t2",
+        "chrom": "chr1",
+        "strand": "+",
+        "txStart": "0",
+        "txEnd": "30",
+        "cdsStart": "10",
+        "cdsEnd": "25",
+        "exonCount": "3",
+        "exonStarts": "0,5,15",
+        "exonEnds": "3,13,29",
+    },
+]
+orf_seq_dict = {
+    "t1::chr1:0-100": "1NNNNNNNNNATGNNNNNNNNNNNN2CAGNNNNNNNNNNNNNNNNNGGNNNNNNNNNNNN3NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNENNNNN",
+    "t2::chr1:0-30": "NNNNNNNNNNATGTNANNNNNNNNNNNNNN",
+}
 
-#####
-expected_return = ["NNNNNNNNATGANNNNNNNNNNNNN"]  # NNN + NNNNNATG + NNNNNNNNNNNNNNN
+test_name = ["t1", "t2"]
 
-
-@pytest.mark.parametrize(
-    "test_dataclass,expected",
-    zip([test_gene_dataclass], expected_return),
-)
-def test_get_exon_seq(test_dataclass, expected):
-    assert get_exon_seq(test_dataclass) == expected
-
-
-#####
-expected_return = [1]
-
-
-@pytest.mark.parametrize(
-    "test_dataclass,expected",
-    zip([test_gene_dataclass], expected_return),
-)
-def test_get_startcodon_exon_num(test_dataclass, expected):
-    assert get_startcodon_exon_num(test_dataclass) == expected
-
-
-#####
-expected_return = [2]
-
-
-@pytest.mark.parametrize(
-    "test_dataclass,expected", zip([test_gene_dataclass], expected_return)
-)
-def test_get_stopcodon_exon_num(test_dataclass, expected):
-    assert get_stopcodon_exon_num(test_dataclass) == expected
-
-
-#####
-exon_seq = ["NNNNNNNNATGANNNNNNNNNNNNN"]
-startcodon_exonindex = [1]
-endcodon_exonindex = [2]
-expected_return = ["ATGANNNNNNNNN"]  # ATG +ANNNNNNNNN
+expected_return = [
+    "1NNNNNNNNNATGNN2CAGNNNNNNNNNNNNNNNNNGGNN3NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNE",
+    "NNNNNNNNATGANNNNNNNNNNNNN",  # NNN + NNNNNATG + NNNNNNNNNNNNNNN
+]
 
 
 @pytest.mark.parametrize(
-    "test_dataclass,exon_seq,start_index,end_index,expected",
+    "test_name,input_genedata,orf_seq_dict,expected",
     zip(
-        [test_gene_dataclass],
+        test_name,
+        [input_genedata] * len(test_name),
+        [orf_seq_dict] * len(test_name),
+        expected_return,
+    ),
+)
+def test_get_exon_seq(test_name, input_genedata, orf_seq_dict, expected):
+    assert (
+        get_exon_seq(set_dataclass(test_name, input_genedata, orf_seq_dict)) == expected
+    )
+
+
+#####
+expected_return = [0, 1]
+
+
+@pytest.mark.parametrize(
+    "test_name,input_genedata,orf_seq_dict,expected",
+    zip(
+        test_name,
+        [input_genedata] * len(test_name),
+        [orf_seq_dict] * len(test_name),
+        expected_return,
+    ),
+)
+def test_get_startcodon_exon_num(test_name, input_genedata, orf_seq_dict, expected):
+    assert (
+        get_startcodon_exon_num(set_dataclass(test_name, input_genedata, orf_seq_dict))
+        == expected
+    )
+
+
+#####
+expected_return = [2, 2]
+
+
+@pytest.mark.parametrize(
+    "test_name,input_genedata,orf_seq_dict,expected",
+    zip(
+        test_name,
+        [input_genedata] * len(test_name),
+        [orf_seq_dict] * len(test_name),
+        expected_return,
+    ),
+)
+def test_get_stopcodon_exon_num(test_name, input_genedata, orf_seq_dict, expected):
+    assert (
+        get_stopcodon_exon_num(set_dataclass(test_name, input_genedata, orf_seq_dict))
+        == expected
+    )
+
+
+#####
+exon_seq = [
+    "1NNNNNNNNNATGNN2CAGNNNNNNNNNNNNNNNNNGGNN3NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNE",
+    "NNNNNNNNATGANNNNNNNNNNNNN",
+]
+startcodon_exonindex = [0, 1]
+endcodon_exonindex = [2, 2]
+expected_return = [
+    "ATGNN2CAGNNNNNNNNNNNNNNNNNGGNN3NNNNNNNNNNNNNNNNNNNNNNNNNNNNN",
+    "ATGANNNNNNNNN",
+]  # ATG +ANNNNNNNNN
+
+
+@pytest.mark.parametrize(
+    "test_name,input_genedata,orf_seq_dict,exon_seq,start_index,end_index,expected",
+    zip(
+        test_name,
+        [input_genedata] * len(test_name),
+        [orf_seq_dict] * len(test_name),
         exon_seq,
         startcodon_exonindex,
         endcodon_exonindex,
         expected_return,
     ),
 )
-def test_get_cdsseq(test_dataclass, exon_seq, start_index, end_index, expected):
-    assert get_cdsseq(test_dataclass, exon_seq, start_index, end_index) == expected
+def test_get_cdsseq(
+    test_name, input_genedata, orf_seq_dict, exon_seq, start_index, end_index, expected
+):
+    assert (
+        get_cdsseq(
+            set_dataclass(test_name, input_genedata, orf_seq_dict),
+            exon_seq,
+            start_index,
+            end_index,
+        )
+        == expected
+    )
