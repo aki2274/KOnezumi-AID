04686237a3aa0e2fa3edab6a75c273b12584b4f7
aki2274 <s2110307@s.tsukuba.ac.jp>
2023-11-22 09:54:47 +0900

    Create make_rtpcr_primer.py

---
+++ src/make_rtpcr_primer.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+import primer3
+
+
+def export_candidate_primer(exon_seq: str) -> dict:
+    result = primer3.bindings.design_primers(
+        seq_args={
+            "SEQUENCE_TEMPLATE": exon_seq,
+        },
+        global_args={
+            "PRIMER_PRODUCT_SIZE_RANGE": [[80, 150], [150, 300]],
+            "PRIMER_MIN_SIZE": 17,
+            "PRIMER_OPT_SIZE": 20,
+            "PRIMER_MAX_SIZE": 25,
+            "PRIMER_PAIR_MAX_DIFF_TM": 4,
+            "PRIMER_OPT_TM": 62.0,
+            "PRIMER_MIN_TM": 60.0,
+            "PRIMER_MAX_TM": 65.0,
+            "PRIMER_MIN_GC": 40.0,
+            "PRIMER_OPT_GC_PERCENT": 50.0,
+            "PRIMER_MAX_GC": 60.0,
+            "PRIMER_MAX_SELF_ANY": 8,
+            "PRIMER_PAIR_MAX_COMPL_ANY": 8,  # プライマー間での3 base以上の相補的配列をここで考慮する？
+            "PRIMER_MAX_SELF_END": 3,
+            "PRIMER_MAX_SELF_END ": 3,  # これでいい？
+            "PRIMER_MAX_POLY_X": 4,
+            "PRIMER_WT_TM_GT": 0.5,
+            "PRIMER_WT_TM_LT": 0.5,
+            "PRIMER_WT_SELF_ANY": 0.5,
+            "PRIMER_WT_SELF_END": 1,
+            "PRIMER_PAIR_WT_DIFF_TM": 0.5,
+            "PRIMER_PAIR_WT_COMPL_ANY": 0.5,
+            "PRIMER_PAIR_WT_COMPL_END": 1,
+        },
+    )
