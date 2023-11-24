6c84a7503dc65ea12c390ec1c92a1f10b3ff11eb
aki2274 <s2110307@s.tsukuba.ac.jp>
2023-11-25 06:10:47 +0900

    配列中の小文字をそのまま小文字として扱うように変更しました。
    テストケースの追加もおこないました。

--- src/get_reverse_complement.py
+++ src/get_reverse_complement.py
@@ -3,5 +3,16 @@ from __future__ import annotations
 
 def get_revcomp(orf_seq: str) -> str:
     """Reverse complement a DNA sequence."""
-    complement = {"A": "T", "C": "G", "G": "C", "T": "A", "N": "N"}
-    return "".join(complement[nt.upper()] for nt in orf_seq[::-1])
+    complement = {
+        "A": "T",
+        "C": "G",
+        "G": "C",
+        "T": "A",
+        "N": "N",
+        "a": "t",
+        "c": "g",
+        "g": "c",
+        "t": "a",
+        "n": "n",
+    }
+    return "".join(complement[nt] for nt in orf_seq[::-1])
--- tests/src/test_get_revcomp.py
+++ tests/src/test_get_revcomp.py
@@ -2,25 +2,27 @@ import pytest
 from src.get_reverse_complement import get_revcomp
 
 # small letter means low complexity region
-input_seq = ["ATGCAAATTTGGGCCC", "NNNNNNNNNN", "ATCGN", "atgc", "nnn", "ATGCatgc"]
+input_seq = [
+    "ATGCAAATTTGGGCCC",
+    "atgcaaatttgggccc",
+    "NNNNNNNNNN",
+    "ATCGN",
+    "atgc",
+    "nnnnn",
+    "ATGCatgc",
+]
 
-expected = ["GGGCCCAAATTTGCAT", "NNNNNNNNNN", "NCGAT", "GCAT", "NNN", "GCATGCAT"]
+expected = [
+    "GGGCCCAAATTTGCAT",
+    "gggcccaaatttgcat",
+    "NNNNNNNNNN",
+    "NCGAT",
+    "gcat",
+    "nnnnn",
+    "gcatGCAT",
+]
 
 
 @pytest.mark.parametrize("seq,expected", zip(input_seq, expected))
 def test_get_revcomp(seq, expected):
     assert get_revcomp(seq) == expected
-
-
-# test the case that the unexpected character is included in the sequence.
-# このエラーのテストが必要かどうかについて検討
-"""
-error_input_seq = ["ATGCQ", "ATGC>", "ATGCw", "ATGC1"]
-
-expecte_error = ["KeyError: 'Q'", "KeyError: '>'", "KeyError: 'W'", "KeyError: '1'"]
-
-
-@pytest.mark.parametrize("seq,expected", zip(error_input_seq, expecte_error))
-def test_get_revcomp_error(seq, expected):
-    assert get_revcomp(seq) == expected
-"""
