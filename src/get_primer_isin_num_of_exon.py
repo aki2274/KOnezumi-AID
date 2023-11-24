6fafd9ac551cc9398ed9e05012eb479b0e1a75cb
aki2274 <s2110307@s.tsukuba.ac.jp>
2023-11-24 19:22:05 +0900

    Update get_primer_isin_num_of_exon.py
    型ヒントが正確でない可能性があるので注釈を加えました。

--- src/get_primer_isin_num_of_exon.py
+++ src/get_primer_isin_num_of_exon.py
@@ -1,5 +1,6 @@
 def get_primer_exon_num(
-    primer_index_list: list[str], exon_range: list[int]
+    primer_index_list: list[int],  # マッパーで得られるプライマーの位置なので型が違うかも。
+    exon_range: list[list[int, int]],
 ) -> list[int]:
     # check which exon the primer is in
     primer_num_list = []
