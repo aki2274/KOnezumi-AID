6199209855a9fbe8d8306f1916fef274a97ebb52
aki2274 <s2110307@s.tsukuba.ac.jp>
2023-11-24 19:26:54 +0900

    Update check_primer_quality.py
    list[dict]型の入力に対応するように書き換えました。

--- src/check_primer_quality.py
+++ src/check_primer_quality.py
@@ -15,18 +15,26 @@ class GeneData:
 
 
 def check_exon_junction(
-    primer_list: list[str], primer_index_list: list[int], exon_range: list[int]
+    candidate_primer_pairs: list[dict],
+    primer_index_list: list[int],  # マッパーで得られるプライマーの位置なので型が違うかも。
+    exon_range: list[int],
 ) -> bool:
     # primer_index_list is the index in exon. if return True, then the primer is in exon junction.
     result = []
-    for primer_num in range(len(primer_index_list)):
-        primer_longth = len(primer_list[primer_num])
+    for left_primer, right_primer in zip(
+        candidate_primer_pairs["left_primer"], candidate_primer_pairs["right_primer"]
+    ):
+        left_primer_longth = len(left_primer)
+        right_primer_longth = len(right_primer)
         bool_list = []
-        # check if the primer is in any exon junction
+        # check if the left primer is in any exon junction
         for exon_num in range(len(exon_range)):
             if exon_range[exon_num][1] in range(
-                primer_index_list[primer_num],
-                primer_index_list[primer_num] + primer_longth + 1,
+                primer_index_list[exon_num],
+                primer_index_list[exon_num] + left_primer_longth + 1,
+            ) or exon_range[exon_num][1] in range(
+                primer_index_list[exon_num],
+                primer_index_list[exon_num] + right_primer_longth + 1,
             ):
                 bool_list.append(True)
             else:
@@ -37,18 +45,23 @@ def check_exon_junction(
 
 
 def get_primer_pair_intron_len(
-    left_primer_num_list: list[int], right_primer_num_list: list[int], ds: GeneData
+    candidate_primer_pairs: list[dict],
+    left_primer_exon_num: list[int],
+    right_primer_exon_num: list[int],
+    ds: GeneData,
 ) -> list[int]:
     # get intron length between primer pairs
     intron_len_list = []
     # the length is not considered the length of the exons in primer pairs.
-    for primer_num in range(len(left_primer_num_list)):
+    for primer_num in range(len(left_primer_exon_num)):
         intron_len = (
-            ds.exon_start_list[right_primer_num_list[primer_num]]
-            - ds.exon_end_list[left_primer_num_list[primer_num]]
+            ds.exon_start_list[right_primer_exon_num[primer_num]]
+            - ds.exon_end_list[left_primer_exon_num[primer_num]]
         )
         intron_len_list.append(intron_len)
     # if the primer pair is in the same exon, the intron length is 0.
     # (the showed number of intron length is negative if primer_num is the same.)
-    intron_natural_len = [max(0, i) for i in intron_len_list]
-    return intron_natural_len
+    for intron_len, primer_data in zip(intron_len_list, candidate_primer_pairs):
+        if intron_len > 0:
+            primer_data["intron_len"] = intron_len
+    return candidate_primer_pairs
