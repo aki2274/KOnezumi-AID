e13617bc9a457484478643f6acc988b78f71955d
aki2274 <s2110307@s.tsukuba.ac.jp>
2023-11-26 00:13:12 +0900

    add test get rtPCR primers

---
+++ tests/src/test_get_rtpcr_primer/test_check_primer_quality.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+from src.get_rtpcr_primer.check_primer_quality import (
+    check_exon_junction,
+    rewrite_primer_pair_intron_len,
+)
---
+++ tests/src/test_get_rtpcr_primer/test_make_rtpcr_primer.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+from src.get_rtpcr_primer.make_rtpcr_primer import (
+    export_candidate_primer,
+    get_candidate_primer_pairs,
+)
+
+exon_seq = ["ATCGATCGATCG", "ATCGATCGATCG", "ATCGATCGATCG"]
