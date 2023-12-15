7ce53745c11856fa56e18fc72af92a1262088566
aki2274 <s2110307@s.tsukuba.ac.jp>
2023-12-15 15:58:01 +0900

    change tests
    srcの書き換えに合わせてテストを変更しました。

--- tests/src/test_get_rtpcr_primer/test_check_primer_quality.py
+++ tests/src/test_get_rtpcr_primer/test_check_primer_quality.py
@@ -7,54 +7,81 @@ from src.get_rtpcr_primer.check_primer_quality import (
 )
 
 
-def test_true_check_exon_junction():
+def test_cross_check_exon_junction():
     True_primer_info = [
         # left primer is in exon junction
         {
-            "primer_score": 999,
+            "left_cross_junction": 0,
+            "right_cross_junction": 0,
             "intron_len": 0,
             "left_primer": "agcaaaagtgtgaagcgccc",
             "right_primer": "atctcgatcaccacgggctg",
-            "left_primer_start": 5,
+            "left_primer_end": 29,
             "right_primer_start": 60,
             "left_primer_exon_num": 0,
             "right_primer_exon_num": 2,
         },
         # right primer is in exon junction
         {
-            "primer_score": 999,
+            "left_cross_junction": 0,
+            "right_cross_junction": 0,
             "intron_len": 0,
             "left_primer": "agcaaaagtgtgaagcgccc",
             "right_primer": "atctcgatcaccacgggctg",
-            "left_primer_start": 15,
+            "left_primer_end": 45,
             "right_primer_start": 49,
             "left_primer_exon_num": 0,
             "right_primer_exon_num": 2,
         },
     ]
     exon_range = [[0, 10], [10, 50], [50, 100]]
-    expected = [True, True]
-    assert check_exon_junction(True_primer_info, exon_range) == expected
-
-
-def test_false_check_exon_junction():
-    False_primer_info = [
+    expected = [
         {
-            "primer_score": 999,
+            "left_cross_junction": 1,
+            "right_cross_junction": 0,
             "intron_len": 0,
             "left_primer": "agcaaaagtgtgaagcgccc",
             "right_primer": "atctcgatcaccacgggctg",
-            "left_primer_start": 27,
+            "left_primer_end": 29,
+            "right_primer_start": 60,
+            "left_primer_exon_num": 0,
+            "right_primer_exon_num": 2,
+        },
+        {
+            "left_cross_junction": 0,
+            "right_cross_junction": 1,
+            "intron_len": 0,
+            "left_primer": "agcaaaagtgtgaagcgccc",
+            "right_primer": "atctcgatcaccacgggctg",
+            "left_primer_end": 45,
+            "right_primer_start": 49,
+            "left_primer_exon_num": 0,
+            "right_primer_exon_num": 2,
+        },
+    ]
+    assert check_exon_junction(True_primer_info, exon_range) == expected
+
+
+def test_not_cross_check_exon_junction():
+    False_primer_info = [
+        {
+            "left_cross_junction": 0,
+            "right_cross_junction": 0,
+            "intron_len": 0,
+            "left_primer": "agcaaaagtgtgaagcgccc",
+            "right_primer": "atctcgatcaccacgggctg",
+            "left_primer_end": 45,
             "right_primer_start": 60,
             "left_primer_exon_num": 0,
             "right_primer_exon_num": 0,
         },
         {
-            "primer_score": 999,
+            "left_cross_junction": 0,
+            "right_cross_junction": 0,
             "intron_len": 1000,
             "left_primer": "agcaaaagtgtgaagcgccc",
             "right_primer": "atctcgatcaccacgggctg",
-            "left_primer_start": 10,
+            "left_primer_end": 8,
             "right_primer_start": 50,
             "left_primer_exon_num": 0,
             "right_primer_exon_num": 2,
@@ -63,7 +90,30 @@ def test_false_check_exon_junction():
 
     exon_range = [[0, 10], [10, 50], [50, 100]]
 
-    expected = [False, False]
+    expected = [
+        {
+            "left_cross_junction": 0,
+            "right_cross_junction": 0,
+            "intron_len": 0,
+            "left_primer": "agcaaaagtgtgaagcgccc",
+            "right_primer": "atctcgatcaccacgggctg",
+            "left_primer_end": 45,
+            "right_primer_start": 60,
+            "left_primer_exon_num": 0,
+            "right_primer_exon_num": 0,
+        },
+        {
+            "left_cross_junction": 0,
+            "right_cross_junction": 0,
+            "intron_len": 1000,
+            "left_primer": "agcaaaagtgtgaagcgccc",
+            "right_primer": "atctcgatcaccacgggctg",
+            "left_primer_end": 8,
+            "right_primer_start": 50,
+            "left_primer_exon_num": 0,
+            "right_primer_exon_num": 2,
+        },
+    ]
     assert check_exon_junction(False_primer_info, exon_range) == expected
 
 
@@ -83,42 +133,46 @@ class GeneData:
 def test_rewrite_primer_pair_intron_len():
     candidate_primer_info = [
         {
-            "primer_score": 999,
+            "left_cross_junction": 0,
+            "right_cross_junction": 0,
             "intron_len": 0,
             "left_primer": "agcaaaagtgtgaagcgccc",
             "right_primer": "atctcgatcaccacgggctg",
-            "left_primer_start": 5,
+            "left_primer_end": 5,
             "right_primer_start": 60,
             "left_primer_exon_num": 0,
             "right_primer_exon_num": 1,
         },
         {
-            "primer_score": 999,
+            "left_cross_junction": 0,
+            "right_cross_junction": 0,
             "intron_len": 0,
             "left_primer": "agcaaaagtgtgaagcgccc",
             "right_primer": "atctcgatcaccacgggctg",
-            "left_primer_start": 5,
+            "left_primer_end": 5,
             "right_primer_start": 60,
             "left_primer_exon_num": 1,
             "right_primer_exon_num": 2,
         },
         {
-            "primer_score": 999,
+            "left_cross_junction": 0,
+            "right_cross_junction": 0,
             "intron_len": 0,
             "left_primer": "agcaaaagtgtgaagcgccc",
             "right_primer": "atctcgatcaccacgggctg",
-            "left_primer_start": 15,
+            "left_primer_end": 15,
             "right_primer_start": 49,
             "left_primer_exon_num": 0,
             "right_primer_exon_num": 2,
         },
         # 0 intron length
         {
-            "primer_score": 999,
+            "left_cross_junction": 0,
+            "right_cross_junction": 0,
             "intron_len": 0,
             "left_primer": "agcaaaagtgtgaagcgccc",
             "right_primer": "atctcgatcaccacgggctg",
-            "left_primer_start": 15,
+            "left_primer_end": 15,
             "right_primer_start": 49,
             "left_primer_exon_num": 0,
             "right_primer_exon_num": 0,
@@ -127,41 +181,45 @@ def test_rewrite_primer_pair_intron_len():
     set_data = GeneData("sample_base", 0, 100, 10, 90, 3, [0, 25, 60], [15, 50, 95])
     expected = [
         {
-            "primer_score": 999,
-            "intron_len": 10,
+            "left_cross_junction": 0,
+            "right_cross_junction": 0,
+            "intron_len": 11,
             "left_primer": "agcaaaagtgtgaagcgccc",
             "right_primer": "atctcgatcaccacgggctg",
-            "left_primer_start": 5,
+            "left_primer_end": 5,
             "right_primer_start": 60,
             "left_primer_exon_num": 0,
             "right_primer_exon_num": 1,
         },
         {
-            "primer_score": 999,
-            "intron_len": 10,
+            "left_cross_junction": 0,
+            "right_cross_junction": 0,
+            "intron_len": 11,
             "left_primer": "agcaaaagtgtgaagcgccc",
             "right_primer": "atctcgatcaccacgggctg",
-            "left_primer_start": 5,
+            "left_primer_end": 5,
             "right_primer_start": 60,
             "left_primer_exon_num": 1,
             "right_primer_exon_num": 2,
         },
         {
-            "primer_score": 999,
-            "intron_len": 45,
+            "left_cross_junction": 0,
+            "right_cross_junction": 0,
+            "intron_len": 46,
             "left_primer": "agcaaaagtgtgaagcgccc",
             "right_primer": "atctcgatcaccacgggctg",
-            "left_primer_start": 15,
+            "left_primer_end": 15,
             "right_primer_start": 49,
             "left_primer_exon_num": 0,
             "right_primer_exon_num": 2,
         },
         {
-            "primer_score": 999,
+            "left_cross_junction": 0,
+            "right_cross_junction": 0,
             "intron_len": 0,
             "left_primer": "agcaaaagtgtgaagcgccc",
             "right_primer": "atctcgatcaccacgggctg",
-            "left_primer_start": 15,
+            "left_primer_end": 15,
             "right_primer_start": 49,
             "left_primer_exon_num": 0,
             "right_primer_exon_num": 0,
