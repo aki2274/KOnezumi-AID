aef8cc60ece028c2e19651f85c9774e33ce9e482
aki2274 <s2110307@s.tsukuba.ac.jp>
2023-12-15 20:53:36 +0900

    Create get_uniqueness.sh
    bowtieを使ってユニークさを検証します

---
+++ src/get_rtpcr_primer/get_uniqueness.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+input_primers="data/reads/candidateprimer.fa"
+
+bowtie -a -v 0 -x data/indexes/mm39 -f "$input_primers" > /tmp/bowtie_result_0.sam
+bowtie -a -v 1 -x data/indexes/mm39 -f "$input_primers" > /tmp/bowtie_result_1.sam
+bowtie -a -v 2 -x data/indexes/mm39 -f "$input_primers" > /tmp/bowtie_result_2.sam
+
+grep -v "^@" /tmp/bowtie_result_0.sam | cut -f 1 | sort | uniq -c > data/uniq/0_miss_counts.txt
+grep -v "^@" /tmp/bowtie_result_1.sam | cut -f 1 | sort | uniq -c >> data/uniq/1_miss_counts.txt
+grep -v "^@" /tmp/bowtie_result_2.sam | cut -f 1 | sort | uniq -c >> data/uniq/2_miss_counts.txt
