91baa53fb8dfcc52a55397a411b9868fcd09630e
aki2274 <s2110307@s.tsukuba.ac.jp>
2023-12-18 14:28:40 +0900

    Update export_fasta.py

--- src/get_rtpcr_primer/export_fasta.py
+++ src/get_rtpcr_primer/export_fasta.py
@@ -2,12 +2,19 @@ from __future__ import annotations
 
 
 def export_fasta(data: list[dict]) -> None:
-    fasta_file = "data/reads/candidateprimer.fa"
+    file_path = "data/reads/candidateprimer.fa"
+    processed_names = set()
 
-    with open(fasta_file, "w") as file:
-        for num, item in enumerate(data, start=1):
+    with open(file_path, "w") as file:
+        for item in data:
             left_primer = item["left_primer"]
             right_primer = item["right_primer"]
 
-            file.write(f">L{num}\n{left_primer}\n")
-            file.write(f">R{num}\n{right_primer}\n")
+            left_primer_name = left_primer
+            if left_primer_name not in processed_names:
+                processed_names.add(left_primer_name)
+                file.write(f">{left_primer_name}\n{left_primer}\n")
+            right_primer_name = right_primer
+            if right_primer_name not in processed_names:
+                processed_names.add(right_primer_name)
+                file.write(f">{right_primer_name}\n{right_primer}\n")
