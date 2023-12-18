5dda313270991173d092be20e3490915d974b5ba
aki2274 <s2110307@s.tsukuba.ac.jp>
2023-12-18 15:46:42 +0900

    Update test_get_uniqueness.py

--- tests/src/test_get_rtpcr_primer/test_get_uniqueness.py
+++ tests/src/test_get_rtpcr_primer/test_get_uniqueness.py
@@ -2,13 +2,8 @@ from __future__ import annotations
 import pytest
 import subprocess
 
+
 # test bowtie's working
-test_shell = """
-    bowtie tests/data/indexes/e_coli tests/data/reads/e_coli_1000.fq > /tmp/bowtie_test.sam
-"""
-subprocess.run(["bash", "-c", test_shell])
-
-
 def read_sam(file_path: str):
     with open(file_path, "r") as f:
         sam = f.readlines()
@@ -16,5 +11,9 @@ def read_sam(file_path: str):
 
 
 def test_get_uniqueness():
+    test_shell = """
+    bowtie tests/data/indexes/e_coli tests/data/reads/e_coli_1000.fq > /tmp/bowtie_test.sam
+    """
+    subprocess.run(["bash", "-c", test_shell])
     expected_sam = read_sam("tests/data/uniq/e_coli_1000.sam")
     assert read_sam("/tmp/bowtie_test.sam") == expected_sam
