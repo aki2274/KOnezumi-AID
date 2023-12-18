1cdcf6f2c2cd8186c17f16f363c0c524ec6226ba
aki2274 <s2110307@s.tsukuba.ac.jp>
2023-12-18 15:54:59 +0900

    Update test_add_uniqueness.py
    pathの指定方法をかえました。

--- tests/src/test_get_rtpcr_primer/test_add_uniqueness.py
+++ tests/src/test_get_rtpcr_primer/test_add_uniqueness.py
@@ -13,3 +13,52 @@ def test_read_uniqueness():
     ]
 
     assert read_uniqueness("tests/data/uniq/test_counts.txt") == expected
+
+
+def test_add_uniqueness():
+    candidate = [
+        {"left_primer": "ATGC", "right_primer": "TCGA"},
+        {"left_primer": "TTTT", "right_primer": "AAAA"},
+        {"left_primer": "CCCC", "right_primer": "GGGG"},
+    ]
+    expected = [
+        {
+            "left_primer": "ATGC",
+            "right_primer": "TCGA",
+            "left_0_uniq": "23",
+            "right_0_uniq": "7",
+            "left_1_uniq": "23",
+            "right_1_uniq": "7",
+            "left_2_uniq": "23",
+            "right_2_uniq": "7",
+        },
+        {
+            "left_primer": "TTTT",
+            "right_primer": "AAAA",
+            "left_0_uniq": "7",
+            "right_0_uniq": "3",
+            "left_1_uniq": "7",
+            "right_1_uniq": "3",
+            "left_2_uniq": "7",
+            "right_2_uniq": "3",
+        },
+        {
+            "left_primer": "CCCC",
+            "right_primer": "GGGG",
+            "left_0_uniq": "12",
+            "right_0_uniq": "0",
+            "left_1_uniq": "12",
+            "right_1_uniq": "0",
+            "left_2_uniq": "12",
+            "right_2_uniq": "0",
+        },
+    ]
+    assert (
+        add_uniqueness(
+            candidate,
+            "tests/data/uniq/test_counts.txt",
+            "tests/data/uniq/test_counts.txt",
+            "tests/data/uniq/test_counts.txt",
+        )
+        == expected
+    )
