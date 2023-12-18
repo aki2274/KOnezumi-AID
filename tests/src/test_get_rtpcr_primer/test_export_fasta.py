c8e41be02cf7476f8e4e82fa86787a9d72121dd9
aki2274 <s2110307@s.tsukuba.ac.jp>
2023-12-18 15:25:49 +0900

    Create test_export_fasta.py

---
+++ tests/src/test_get_rtpcr_primer/test_export_fasta.py
@@ -0,0 +1,24 @@
+import pytest
+from src.get_rtpcr_primer.export_fasta import export_fasta
+
+
+def test_exprt_fasta():
+    data = [
+        {
+            "left_cross_junction": 0,
+            "right_cross_junction": 0,
+            "intron_len": 0,
+            "left_primer": "agcaaaagtgtgaagcgccc",
+            "right_primer": "atctcgatcaccacgggctg",
+            "left_primer_end": 29,
+            "right_primer_start": 60,
+            "left_primer_exon_num": 0,
+            "right_primer_exon_num": 2,
+        }
+    ]
+    export_fasta(data)
+    with open("data/reads/candidateprimer.fa", "r") as file:
+        assert (
+            file.read()
+            == ">agcaaaagtgtgaagcgccc\nagcaaaagtgtgaagcgccc\n>atctcgatcaccacgggctg\natctcgatcaccacgggctg\n"
+        )
