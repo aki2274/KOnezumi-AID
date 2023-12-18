155541fdce789cbb9ca9f1cf3d324760a63e51e5
Akihiro Kuno <akikuno@users.noreply.github.com>
2023-12-19 05:24:47 +0900

    add `raise Exception` to notice when bowtie not found

--- tests/src/test_get_rtpcr_primer/test_get_uniqueness.py
+++ tests/src/test_get_rtpcr_primer/test_get_uniqueness.py
@@ -12,7 +12,13 @@ def read_sam(file_path: str):
 
 def test_get_uniqueness():
     test_shell = "bowtie tests/data/indexes/e_coli tests/data/reads/e_coli_1000.fq > /tmp/bowtie_test.sam"
-    process = subprocess.Popen(["bash", "-c", test_shell])
-    process.communicate()
+
+    process = subprocess.Popen(["bash", "-c", test_shell], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    _, stderr = process.communicate()
+
+    # 標準エラー出力があれば表示する
+    if stderr:
+        raise Exception(stderr.decode())
+
     expected_sam = read_sam("tests/data/uniq/e_coli_1000.sam")
     assert read_sam("/tmp/bowtie_test.sam") == expected_sam
