d7d68bc8a18a38b06685822a05608f233ead820f
aki2274 <s2110307@s.tsukuba.ac.jp>
2023-12-18 23:20:19 +0900

    Update test_get_uniqueness.py
    subprocessをrunではなくPopenに変化させました。

--- tests/src/test_get_rtpcr_primer/test_get_uniqueness.py
+++ tests/src/test_get_rtpcr_primer/test_get_uniqueness.py
@@ -12,6 +12,7 @@ def read_sam(file_path: str):
 
 def test_get_uniqueness():
     test_shell = "bowtie tests/data/indexes/e_coli tests/data/reads/e_coli_1000.fq > /tmp/bowtie_test.sam"
-    subprocess.run(["bash", "-c", test_shell])
+    process = subprocess.Popen(["bash", "-c", test_shell])
+    process.communicate()
     expected_sam = read_sam("tests/data/uniq/e_coli_1000.sam")
     assert read_sam("/tmp/bowtie_test.sam") == expected_sam
