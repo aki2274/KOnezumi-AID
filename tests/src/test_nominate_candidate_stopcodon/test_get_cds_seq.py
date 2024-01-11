ee44b58691831dcdf8be9e0dafa6b2a3c22fd856
aki2274 <s2110307@s.tsukuba.ac.jp>
2024-01-11 18:47:35 +0900

    get_cds_seq to generate_cds_seq

--- tests/src/test_nominate_candidate_stopcodon/test_get_cds_seq.py
+++ tests/src/test_nominate_candidate_stopcodon/test_get_cds_seq.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 import pytest
 from dataclasses import dataclass
 from create_gene_dataclass import set_dataclass
-from src.nominate_candidate_stopcodon.get_cds_seq import (
+from nominate_candidate_stopcodon.generate_cds_seq import (
     get_exon_seq,
     get_startcodon_exon_num,
     get_stopcodon_exon_num,
