ded002450af7c459ab21a982460292d679c04241
aki2274 <s2110307@s.tsukuba.ac.jp>
2024-01-17 11:33:59 +0900

    Update descriptions.

--- src/nominate_candidate_stopcodon/translate_index_in_exon_to_orf.py
+++ src/nominate_candidate_stopcodon/translate_index_in_exon_to_orf.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from dataclasses import dataclass
 import numpy as np
+from src.get_range_of_exon import get_exon_range
 
 
 @dataclass
@@ -21,69 +22,60 @@ class GeneData:
 # 　エクソンごとの範囲を取得、candidate stopcodon が何番目のエクソンか知る、
 
 
-def get_exon_range(ds: GeneData):
-    # エクソンごとの範囲を取得
-    exon_range_list = []
-    num = 0
-    for s in range(len(ds.exon_start_list)):
-        element_list = []
-        element_list.append(num)
-        num += ds.exon_end_list[s] - ds.exon_start_list[s]
-        element_list.append(num)
-        exon_range_list.append(element_list)
-
-    return exon_range_list
-
-
-# cdsでのindexをエクソンのindexに変換
+# the candidate indexes in cds are convereted to indexes in exon
 def translate_incds_index_to_exon(
     ds: GeneData,
     candidate_stopcodon_cds_index_list: list[int],
-    exon_range_list: list[int],
     cdsStart_exon_index: int,
 ) -> list[int]:
+    # get the exon range
+    exon_range_list = get_exon_range(ds)
     candidate_index_array = np.array(candidate_stopcodon_cds_index_list)
-    # エクソン上でのエクソンのはじめからstartcodonが含まれるエクソンの先頭までの距離
+    # the distance from the start of orf to the exon which has startcodon
     dist_to_startcodon_exon = exon_range_list[cdsStart_exon_index][0]
-    # エクソン上でのエクソンのはじめからstartcodon(cdsStart)までの距離。
+    # the distance from the start of the exon(has startcodn) to the startcodon(cdsStart)
     dist_to_startcodon = (
         ds.cdsStart
         - int(ds.exon_start_list[cdsStart_exon_index])
         + dist_to_startcodon_exon
     )
-    # ここでエクソン上で何番目かになってる（cdsからエクソン上に）
-    result = (candidate_index_array + dist_to_startcodon).tolist()
-    return result
+    candidate_codon_index_inexon = (candidate_index_array + dist_to_startcodon).tolist()
+    return candidate_codon_index_inexon
 
 
-def get_exonnum_of_candidate(
-    candidate_stopcodon_index_inexon: list[int], exon_range_list
-):
-    # 何番目のエクソンであるかのリストを作成
+def get_exonnum_of_candidate(candidate_codon_index_inexon: list[int], exon_range_list):
+    # create a list of exon number of candidate codon is in
     exon_index_list = []
-    for t in range(len(candidate_stopcodon_index_inexon)):
+    for t in range(len(candidate_codon_index_inexon)):
         for s in range(len(exon_range_list)):
             if (
                 exon_range_list[s][0]
-                <= candidate_stopcodon_index_inexon[t]
+                <= candidate_codon_index_inexon[t]
                 <= exon_range_list[s][1]
             ):
                 exon_index_list.append(s)
     return exon_index_list
 
 
-# 足すべき数値を取得、足す
 def translate_index_in_exon_to_orf(
     ds: GeneData,
-    candidate_codon_index_inexon_list: list[int],
-    exon_range_list: list[int],
-    exon_index_list: list[int],
+    candidate_stopcodon_cds_index_list: list[int],
+    cdsStart_exon_index: int,
 ) -> int:
-    # genome上でのcandidatestopcodonを含むexonの先頭までの距離を取得、exon分を2度足しているので引く
+    # get the sum of intron length from the orf start to the exon which has candidate codon
+    exon_range_list = get_exon_range(ds)
+    candidate_codon_index_inexon_list = translate_incds_index_to_exon(
+        ds, candidate_stopcodon_cds_index_list, cdsStart_exon_index
+    )
+    exon_index_list = get_exonnum_of_candidate(
+        candidate_codon_index_inexon_list, exon_range_list
+    )
+
     add_num_to_genome_index = [
         (ds.exon_start_list[i] - ds.txStart - exon_range_list[i][0])
         for i in exon_index_list
     ]
+    # add intron length abd (exon length + the length from exon which has candidate codon to candidate) to get the index of candidate codon in genome
     candidate_stopcodon_index = np.array(candidate_codon_index_inexon_list) + np.array(
         add_num_to_genome_index
     )
