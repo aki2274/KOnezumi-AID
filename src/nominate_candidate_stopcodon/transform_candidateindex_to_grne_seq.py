9ab3c92605a3457b3501ad5e8979d41eaaf8b88e
aki2274 <s2110307@s.tsukuba.ac.jp>
2024-01-17 12:48:34 +0900

    Create transform_candidateindex_to_grne_seq.py

---
+++ src/nominate_candidate_stopcodon/transform_candidateindex_to_grne_seq.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+from dataclasses import dataclass
+
+
+@dataclass
+class GeneData:
+    orf_seq: str
+    txStart: int
+    txend: int
+    cdsStart: int
+    cdsEnd: int
+    exonCount: int
+    exon_start_list: list[int]
+    exon_end_list: list[int]
