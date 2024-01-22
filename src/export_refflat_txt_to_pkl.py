25abb0d6102d5a5e751102f85dc03dd3d0aa72e1
aki2274 <s2110307@s.tsukuba.ac.jp>
2024-01-22 13:12:32 +0900

    Update export_refflat_txt_to_pkl.py

--- src/export_refflat_txt_to_pkl.py
+++ src/export_refflat_txt_to_pkl.py
@@ -4,14 +4,8 @@ import pickle
 
 
 def read_refFlat(path_refFlat: str) -> list[str]:
-    result = []
     with open(path_refFlat, "r") as file:
-        for line in file:
-            line = line.strip().split("\t")
-            # Remove the trailing comma
-            line = [element.strip(",") for element in line]
-            result.append(line)
-    return result
+        return [line.strip().split("\t") for line in file]
 
 
 def built_gene_dataframe(path_refFlat: str) -> pd.DataFrame:
@@ -30,14 +24,12 @@ def built_gene_dataframe(path_refFlat: str) -> pd.DataFrame:
         "exonEnds",
     ]
     results = [dict(zip(key_names, value)) for value in values_refflat]
-    df = pd.DataFrame(results)
-    return df
+    return pd.DataFrame(results)
 
 
 def export_genedata_pkl(
     gene_dataframe: pd.DataFrame, path_output: str = "refFlat_genedata.pkl"
 ) -> None:
-    # Remove duplicates of transcriptions
     duplicates_df = gene_dataframe[gene_dataframe.duplicated("name", keep=False)]
     unique_df = gene_dataframe.drop_duplicates("name", keep=False)
     export_df = unique_df[~unique_df["geneName"].isin(duplicates_df["geneName"])]
