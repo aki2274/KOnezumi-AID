defbb45259807d43d21c16268ff02b0c9cd06974
aki2274 <s2110307@s.tsukuba.ac.jp>
2024-02-10 23:48:44 +0900

    gRNAの23塩基が取り出されるようにしました。

--- src/nominate_spliceside_guide/search_candidate.py
+++ src/nominate_spliceside_guide/search_candidate.py
@@ -1,18 +1,21 @@
 from __future__ import annotations
 from src.create_gene_dataclass import GeneData
+from typing import List, Dict
 
 
-def search_candidate(ds: GeneData):
-    acceptor_cand_rna = []
-    donor_cand_rna = []
-    for s in ds.exon_start_list:
-        search_seq = ds.orf_seq[s - 22 : s + 3]
-        # if "GG" in 0~5 position, it is acceptor candidate
-        if "GG" in search_seq[:4]:
-            acceptor_cand_rna.append(search_seq)
+def search_candidate(ds: GeneData) -> List[Dict[str, int]]:
+    acceptor_candidates = [
+        {"seq": ds.orf_seq[s - 22: s + 3][ds.orf_seq[s - 22: s + 3].index("GG"): ds.orf_seq[s - 22: s + 3].index("GG") + 23],
+         "exon_num": i + 2}
+        for i, s in enumerate(ds.exon_start_list[1:])
+        if "GG" in ds.orf_seq[s - 22: s + 3][:4]
+    ]
 
-    for e in ds.exon_end_list[:-1]:
-        search_seq = ds.orf_seq[e - 22 : e + 3]
-        if "GG" in search_seq[:4]:
-            donor_cand_rna.append(search_seq)
-    return acceptor_cand_rna, donor_cand_rna
+    donor_candidates = [
+        {"seq": ds.orf_seq[e - 21: e + 4][ds.orf_seq[e - 21: e + 4].index("GG"): ds.orf_seq[e - 21: e + 4].index("GG") + 23],
+         "exon_num": i + 1}
+        for i, e in enumerate(ds.exon_end_list[:-1])
+        if "GG" in ds.orf_seq[e - 21: e + 4][:4]
+    ]
+
+    return acceptor_candidates, donor_candidates
