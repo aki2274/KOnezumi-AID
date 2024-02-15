f19c2d4aa1f5933d45467038968c817a21225adf
aki2274 <s2110307@s.tsukuba.ac.jp>
2024-02-15 12:20:25 +0900

    Correct searching PAM seq

--- src/nominate_spliceside_guide/search_candidate.py
+++ src/nominate_spliceside_guide/search_candidate.py
@@ -5,17 +5,31 @@ from typing import List, Dict
 
 def search_candidate(ds: GeneData) -> List[Dict[str, int]]:
     acceptor_candidates = [
-        {"seq": ds.orf_seq[s - 22: s + 3][ds.orf_seq[s - 22: s + 3].index("GG"): ds.orf_seq[s - 22: s + 3].index("GG") + 23],
-         "exon_num": i + 2}
+        {
+            "seq": ds.orf_seq[s - 22 : s + 3][
+                ds.orf_seq[s - 22 : s + 3]
+                .index("CC") : ds.orf_seq[s - 22 : s + 3]
+                .index("CC")
+                + 23
+            ],
+            "exon_num": i + 2,
+        }
         for i, s in enumerate(ds.exon_start_list[1:])
-        if "GG" in ds.orf_seq[s - 22: s + 3][:4]
+        if "CC" in ds.orf_seq[s - 22 : s + 3][:4]
     ]
 
     donor_candidates = [
-        {"seq": ds.orf_seq[e - 21: e + 4][ds.orf_seq[e - 21: e + 4].index("GG"): ds.orf_seq[e - 21: e + 4].index("GG") + 23],
-         "exon_num": i + 1}
+        {
+            "seq": ds.orf_seq[e - 21 : e + 4][
+                ds.orf_seq[e - 21 : e + 4]
+                .index("CC") : ds.orf_seq[e - 21 : e + 4]
+                .index("CC")
+                + 23
+            ],
+            "exon_num": i + 1,
+        }
         for i, e in enumerate(ds.exon_end_list[:-1])
-        if "GG" in ds.orf_seq[e - 21: e + 4][:4]
+        if "CC" in ds.orf_seq[e - 21 : e + 4][:4]
     ]
 
     return acceptor_candidates, donor_candidates
