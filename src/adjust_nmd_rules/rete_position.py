89ced6a0fe54c0d58f62398f9681c51037117807
aki2274 <s2110307@s.tsukuba.ac.jp>
2024-03-01 14:41:06 +0900

    Update rete_position.py

--- src/adjust_nmd_rules/rete_position.py
+++ src/adjust_nmd_rules/rete_position.py
@@ -24,43 +24,19 @@ def label_in_start_150bp(cand_grna: list[dict], ds: GeneData) -> list[dict]:
     return result
 
 
-"""
-# the case of 1 exon
-# Targeting exons whose sequences differ by more than half of the coding region
+# the case of single exon
 def label_in_front_half(cand_grna: list[dict], ds: GeneData) -> list[dict]:
-    result = cand_grna.copy()
-    cds = generate_cdsseq(ds)
-    for grna in result:
-        if "ct_seq" in grna:
-            if re.search(grna["ct_seq"], cds).start() + 3 > len(cds) / 2:
-                grna["back_half"] = True
-            else:
-                grna["back_half"] = False
-        elif "ga_seq" in grna:
-            if re.search(grna["ga_seq"], cds).start() + 19 > len(cds) / 2:
-                grna["back_half"] = True
-            else:
-                grna["back_half"] = False
-    return result
-"""
-
-
-def label_in_front_half(cand_grna: list[dict], ds: GeneData) -> list[dict]:
-    result = cand_grna.copy()
+    result = []
     cds = generate_cdsseq(ds)
     for grna in cand_grna:
         if "ct_seq" in grna:
             ptc_index = re.search(grna["ct_seq"], cds)
             if ptc_index is not None and ptc_index.start() + 3 < len(cds) / 2:
-                grna["front_half"] = True
-            else:
-                grna["front_half"] = False
+                result.append(grna)
         elif "ga_seq" in grna:
             ptc_index = re.search(grna["ga_seq"], cds)
             if ptc_index is not None and ptc_index.start() + 19 < len(cds) / 2:
-                grna["front_half"] = True
-            else:
-                grna["front_half"] = False
+                result.append(grna)
     return result
 
 
