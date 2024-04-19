5386f714986409bd539cc1205b78b2fbe930e521
aki2274 <s2110307@s.tsukuba.ac.jp>
2024-04-19 16:20:55 +0900

    同一スプライスサイトで複数ガイドが組めるときにすべて表示するようにしました。

--- src/konezumiaid/nominate_spliceside_guide/search_candidate.py
+++ src/konezumiaid/nominate_spliceside_guide/search_candidate.py
@@ -4,31 +4,24 @@ from konezumiaid.create_gene_dataclass import GeneData
 
 def search_candidate(ds: GeneData) -> tuple[list[dict[int, str], list[dict[int, str]]]]:
     acceptor_cands = [
-        {
-            "seq": ds.orf_seq[s - 22 : s + 3][
-                ds.orf_seq[s - 22 : s + 3]
-                .index("CC") : ds.orf_seq[s - 22 : s + 3]
-                .index("CC")
-                + 23
-            ],
-            "exon_num": i + 2,
-        }
-        for i, s in enumerate(ds.exon_start_list[1:])
-        if "CC" in ds.orf_seq[s - 22 : s + 3][:4] and "AG" in ds.orf_seq[s - 2 : s]
-    ]
+    {
+        "seq": ds.orf_seq[s - 22 : s + 3][cc_idx : cc_idx + 23],
+        "exon_num": i + 2
+    }
+    for i,s in enumerate(ds.exon_start_list[1:])
+    if "CC" in ds.orf_seq[s - 22 : s + 3][:4] and "AG" in ds.orf_seq[s - 2 : s]
+    for cc_idx in [idx for idx in range(3) if ds.orf_seq[s - 22 : s + 3][idx : idx + 2] == "CC"]
+]
+
 
     donor_cands = [
         {
-            "seq": ds.orf_seq[e - 21 : e + 4][
-                ds.orf_seq[e - 21 : e + 4]
-                .index("CC") : ds.orf_seq[e - 21 : e + 4]
-                .index("CC")
-                + 23
-            ],
+            "seq": ds.orf_seq[e - 21 : e + 4][cc_idx : cc_idx + 23],
             "exon_num": i + 1,
         }
         for i, e in enumerate(ds.exon_end_list[:-1])
         if "CC" in ds.orf_seq[e - 21 : e + 4][:4] and "GT" in ds.orf_seq[e : e + 2]
+        for cc_idx in [idx for idx in range(3) if ds.orf_seq[e - 21 : e + 4][idx : idx + 2] == "CC"]
     ]
 
     acceptor_candidates = [
