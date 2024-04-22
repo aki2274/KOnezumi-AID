3e1a4d528afc9fa0b36d8d4941a55ccfed8d56cc
aki2274 <s2110307@s.tsukuba.ac.jp>
2024-04-22 14:59:01 +0900

    引数名を変更しました。

--- src/konezumiaid/adjust_nmd_rules/rete_position.py
+++ src/konezumiaid/adjust_nmd_rules/rete_position.py
@@ -17,8 +17,8 @@ def create_candidates_list_dict(ct_cand: list[str], ga_cand: list[str]) -> list[
     return candidates_list
 
 
-def label_in_start_150bp(cand_grna: list[dict], transcript_record: GeneData) -> list[dict]:
-    candidate_ = cand_grna.copy()
+def label_in_start_150bp(candidate: list[dict], transcript_record: GeneData) -> list[dict]:
+    candidate_ = candidate.copy()
     for grna in candidate_:
         if "ct_seq" in grna:
             #Add +1 because the position of PTC is +1~3 from the guide start.
@@ -36,11 +36,11 @@ def label_in_start_150bp(cand_grna: list[dict], transcript_record: GeneData) -> 
 
 
 # the case of single exon
-def eliminate_in_front_half(cand_grna: list[dict], ds: GeneData) -> list[dict]:
+def eliminate_in_front_half(candidate: list[dict], transcript_record: GeneData) -> list[dict]:
     # check if the PTC is not in the front half of the CDS
     removed = []
-    cds = generate_cdsseq(ds)
-    for grna in cand_grna:
+    cds = generate_cdsseq(transcript_record)
+    for grna in candidate:
         if "ct_seq" in grna:
             ptc_index = re.search(grna["ct_seq"], cds)
             if ptc_index is not None and ptc_index.start() + 3 < len(cds) / 2:
@@ -53,39 +53,39 @@ def eliminate_in_front_half(cand_grna: list[dict], ds: GeneData) -> list[dict]:
 
 
 # the case of multi exon
-def eliminate_in_last_exon(cand_grna: list[str], ds: GeneData) -> list[str]:
+def eliminate_in_last_exon(candidate: list[str], transcript_record: GeneData) -> list[str]:
     removed = []
-    for grna in cand_grna:
+    for grna in candidate:
         if "ct_seq" in grna:
             if (
-                re.search(grna["ct_seq"], ds.orf_seq).start() + 1
-                <= ds.exon_start_list[-1]
+                re.search(grna["ct_seq"], transcript_record.orf_seq).start() + 1
+                <= transcript_record.exon_start_list[-1]
             ):
                 removed.append(grna)
         elif "ga_seq" in grna:
             if (
-                re.search(grna["ga_seq"], ds.orf_seq).start() + 16
-                <= ds.exon_start_list[-1]
+                re.search(grna["ga_seq"], transcript_record.orf_seq).start() + 16
+                <= transcript_record.exon_start_list[-1]
             ):
                 removed.append(grna)
     return removed
 
 
-def label_in_50bp_from_LEJ(cand_grna: list[dict], ds: GeneData) -> list[dict]:
-    candidate_ = cand_grna.copy()
+def label_in_50bp_from_LEJ(candidate: list[dict], transcript_record: GeneData) -> list[dict]:
+    candidate_ = candidate.copy()
     for grna in candidate_:
         if "ct_seq" in grna:
             if (
-                re.search(grna["ct_seq"], ds.orf_seq).start() + 1
-                >= ds.exon_end_list[-2] - 50
+                re.search(grna["ct_seq"], transcript_record.orf_seq).start() + 1
+                >= transcript_record.exon_end_list[-2] - 50
             ):
                 grna["in_50bp_from_LEJ"] = True
             else:
                 grna["in_50bp_from_LEJ"] = False
         elif "ga_seq" in grna:
             if (
-                re.search(grna["ga_seq"], ds.orf_seq).start() + 16
-                >= ds.exon_end_list[-2] - 50
+                re.search(grna["ga_seq"], transcript_record.orf_seq).start() + 16
+                >= transcript_record.exon_end_list[-2] - 50
             ):
                 grna["in_50bp_from_LEJ"] = True
             else:
