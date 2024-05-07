ff66a5909d1ddb01c6e6e3713ab765ef2fa2c2b0
aki2274 <s2110307@s.tsukuba.ac.jp>
2024-05-07 10:11:24 +0900

    Update generate_cds_seq.py

--- src/konezumiaid/nominate_ptc_guide/generate_cds_seq.py
+++ src/konezumiaid/nominate_ptc_guide/generate_cds_seq.py
@@ -40,38 +40,42 @@ def get_stopcodon_exon_index(transcript_recod: GeneData) -> int:
 
 
 def generate_cdsseq(transcript_recod: GeneData) -> str:
+    """Generate CDS sequence"""
     exon_seq = generate_exon_seq(transcript_recod)
-    startcodon_exon_num = get_startcodon_exon_index(transcript_recod)
-    endcodon_exon_num = get_stopcodon_exon_index(transcript_recod)
+    startcodon_index = get_startcodon_exon_index(transcript_recod)
+    stopcodon_index = get_stopcodon_exon_index(transcript_recod)
 
-    stopcodon_index = (
-        transcript_recod.exon_end_list[endcodon_exon_num] - transcript_recod.cdsEnd
+    stopcodon_distance_from_exonend = (
+        transcript_recod.exon_end_list[stopcodon_index] - transcript_recod.cdsEnd
     )
-    if stopcodon_index == 0:  # if stopcodon is in the last exon
-        exon_seq_to_stopcodon = exon_seq
-    elif (
-        endcodon_exon_num - transcript_recod.exonCount + 1 == 0
-    ):  # if stopcodon is in the last exon
-        exon_seq_to_stopcodon = exon_seq[:-stopcodon_index]
-    else:  # if stopcodon is not in the last exon. (add the len of exons after stopcodon)
-        for s in range(1, transcript_recod.exonCount - endcodon_exon_num):
-            stopcodon_index += (
-                transcript_recod.exon_end_list[endcodon_exon_num + s]
-                - transcript_recod.exon_start_list[endcodon_exon_num + s]
-            )
-        exon_seq_to_stopcodon = exon_seq[
-            :-stopcodon_index
-        ]  # get seq from exon_seq start to stopcodon
-    startcodon_index = (
-        transcript_recod.cdsStart
-        - transcript_recod.exon_start_list[startcodon_exon_num]
+    if stopcodon_distance_from_exonend == 0:  # the case of not having 3'UTR
+        seq_to_stopcodon = exon_seq
+    else:  # the case of having 3'UTR
+        if (
+            stopcodon_index - transcript_recod.exonCount + 1 == 0
+        ):  # the case of stopcodon is in the last exon
+            seq_to_stopcodon = exon_seq[:-stopcodon_distance_from_exonend]
+        else:  # if stopcodon is not in the last exon. (add the len of exons after the exon that has stopcodon)
+            for s in range(1, transcript_recod.exonCount - stopcodon_index):
+                stopcodon_distance_from_last_exonend = stopcodon_distance_from_exonend
+                stopcodon_distance_from_last_exonend += (
+                    transcript_recod.exon_end_list[stopcodon_index + s]
+                    - transcript_recod.exon_start_list[stopcodon_index + s]
+                )
+            seq_to_stopcodon = exon_seq[:-stopcodon_distance_from_exonend]
+
+    startcodon_distance_from_exonstart = (
+        transcript_recod.cdsStart - transcript_recod.exon_start_list[startcodon_index]
     )
-    if startcodon_exon_num == 0:  # if startcodon is in the first exon
-        cds_seq = exon_seq_to_stopcodon[startcodon_index:]
+    if startcodon_index == 0:  # the case of startcodon is in the first exon
+        cds_seq = seq_to_stopcodon[startcodon_distance_from_exonstart:]
     else:  # if startcodon is not in the first exon. (add the len of exons before startcodon)
-        for s in range(startcodon_exon_num):
-            startcodon_index += (
+        for s in range(startcodon_index):
+            startcodon_distance_from_first_exonstart = (
+                startcodon_distance_from_exonstart
+            )
+            startcodon_distance_from_first_exonstart += (
                 transcript_recod.exon_end_list[s] - transcript_recod.exon_start_list[s]
             )
-        cds_seq = exon_seq_to_stopcodon[startcodon_index:]
+        cds_seq = seq_to_stopcodon[startcodon_distance_from_exonstart:]
     return cds_seq
