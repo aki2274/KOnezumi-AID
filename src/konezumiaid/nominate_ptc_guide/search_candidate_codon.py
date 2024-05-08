20f0f1e55e33ab5ac715c24f1eafb666066abc8d
aki2274 <s2110307@s.tsukuba.ac.jp>
2024-05-08 23:17:04 +0900

    関数名、変数名の変更を行いました。

--- src/konezumiaid/nominate_ptc_guide/search_candidate_codon.py
+++ src/konezumiaid/nominate_ptc_guide/search_candidate_codon.py
@@ -2,10 +2,10 @@ from __future__ import annotations
 import re
 
 
-def search_candidate_index(cds_seq: str) -> list:
+def search_candidate_codon_position(cds_seq: str) -> list:
     # search the position of candidate PTC (the index of C in CAA, CAG, CGA, or T in TGG)
-    matches = re.finditer(r"(?=(CAA)|(?=(CAG))|(?=(CGA))|(?=(TGG)))", cds_seq)
-    candidate_codon_index_list = [
-        match.start() for match in matches if (match.start() % 3) == 0
+    candidate_codons = re.finditer(r"(?=(CAA)|(?=(CAG))|(?=(CGA))|(?=(TGG)))", cds_seq)
+    candidate_codon_positions = [
+        codon.start() for codon in candidate_codons if (codon.start() % 3) == 0
     ]
-    return candidate_codon_index_list
+    return candidate_codon_positions
