b410578d0e6f0bfabe37fa75eaf220e2344968c6
aki2274 <s2110307@s.tsukuba.ac.jp>
2024-05-09 12:27:23 +0900

    Update main.py

--- src/konezumiaid/nominate_ptc_guide/main.py
+++ src/konezumiaid/nominate_ptc_guide/main.py
@@ -31,57 +31,52 @@ def nominate_candidate_stopcodon(
     transcript_recod: GeneData,
 ) -> list[dict]:
     """
-    Export candidate gRNA.
+    Export candidate gRNA, that can induce PTC by using Target-AID.
 
     Args:
-        transcript_name (str): transcript name.
-        refflat_path (Path): created refflat pkl file.
-        seq_path (Path): created seq dict pkl file.
+        transcript_recod (GeneData): A GeneData object representing the transcript record.
     Returns:
         list[dict]: the candidate gRNA.
-
-    Example:
-        >>> transcript_name = "NM_001011874"
-        >>> refflat_path = Path("data", "refFlat_genedata_sorted.pkl")
-        >>> seq_path = Path("data", "sorted_seq_dict.pkl")
-        >>> main(transcript_name)
-        ['CCAGTATCAGCCCACTTGGTAGG', 'GACAACTATGTAAAGAGACTTGG'], ['CCACTTATTCTCAGATTTTGGGG', 'CCACTTCATTGATGCTACTGGTT']
     """
     # 1. generate cds seq
     cds_seq = generate_cdsseq(transcript_recod)
     # 2. search candidate index
     candidate_ptc_index = search_candidate_codon_position(cds_seq)
     # 3. get startcodon exon number
-    cdsStart_exon_index = get_startcodon_exon_index(transcript_recod)
+    exon_index = get_startcodon_exon_index(transcript_recod)
     # 4. translate candidate index in exon to index in genome
-    candidate_ptc_index_in_orf = translate_position_in_splicedexon_to_orf(
-        transcript_recod, candidate_ptc_index, cdsStart_exon_index
+    candidate_ptc_codon_positions = translate_position_in_splicedexon_to_orf(
+        transcript_recod, candidate_ptc_index, exon_index
     )
     # 5,find ct target seq
-    ct_target_seq = search_c_to_t_guide_seq(transcript_recod.orf_seq)
+    all_ct_guide_seq = search_c_to_t_guide_seq(transcript_recod.orf_seq)
     # 6. find ga target seq
-    ga_target_seq = search_g_to_a_guide_seq(transcript_recod.orf_seq)
+    all_ga_guide_seq = search_g_to_a_guide_seq(transcript_recod.orf_seq)
     # 7. transform ct guideseq to index
-    ct_guideseq_index = transform_c_to_t_guideseq_to_position(
-        transcript_recod.orf_seq, ct_target_seq
+    editable_c_positions = transform_c_to_t_guideseq_to_position(
+        transcript_recod.orf_seq, all_ct_guide_seq
     )
     # 8. transform ga guideseq to index
-    ga_guideseq_index = transform_g_to_a_guideseq_to_position(
-        transcript_recod.orf_seq, ga_target_seq
+    editable_codon_T_positions = transform_g_to_a_guideseq_to_position(
+        transcript_recod.orf_seq, all_ga_guide_seq
     )
     # 9. compare candidate index and guideseq index,then export candidate if the same index
-    ct_candidate_index = [
-        index for index in candidate_ptc_index_in_orf if index in ct_guideseq_index
+    ct_candidate_positions = [
+        index
+        for index in candidate_ptc_codon_positions
+        if index in editable_c_positions
     ]
-    ga_candidate_index = [
-        index for index in candidate_ptc_index_in_orf if index in ga_guideseq_index
+    ga_candidate_positions = [
+        index
+        for index in candidate_ptc_codon_positions
+        if index in editable_codon_T_positions
     ]
     # 10. transform candidate index to candidate gRNA
     ct_candidate_grna = extract_c_to_t_grna_from_position(
-        transcript_recod, ct_candidate_index
+        transcript_recod, ct_candidate_positions
     )
     ga_candidate_grna = extract_g_to_a_grna_from_position(
-        transcript_recod, ga_candidate_index
+        transcript_recod, ga_candidate_positions
     )
 
     return ct_candidate_grna, ga_candidate_grna
