5da0e4bfe6ea00c0aebbbfa0d781b45e321eb1e7
aki2274 <s2110307@s.tsukuba.ac.jp>
2024-06-03 17:22:59 +0900

    インデックスを修正しました。

--- src/konezumiaid/get_rtpcr_primer/rate_quality.py
+++ src/konezumiaid/get_rtpcr_primer/rate_quality.py
@@ -1,31 +1,26 @@
 from __future__ import annotations
 from konezumiaid.create_gene_dataclass import GeneData
 
+
 def is_crossing_juncion(
-    primer3_result: list[dict],
+    candidate_primers: list[dict],
     exon_range: list[tuple[int, int]],
 ) -> list[dict]:
-    validated_primers = primer3_result.copy()
-    
+    validated_primers = candidate_primers.copy()
+
     for primer_pair in validated_primers:
         left_length = len(primer_pair["left_seq"])
         right_length = len(primer_pair["right_seq"])
         left_end = primer_pair["left_end"]
         right_start = primer_pair["right_start"]
 
-        is_crossing_left = any(
-            (left_end - left_length < exon_end <= left_end)
-            for _, exon_end in exon_range
-        )
+        is_crossing_left = any((left_end - left_length < exon_end <= left_end) for _, exon_end in exon_range)
 
-        is_crossing_right = any(
-            (right_start < exon_end  <= right_start + right_length)
-            for _, exon_end in exon_range
-        )
-        
+        is_crossing_right = any((right_start < exon_end <= right_start + right_length) for _, exon_end in exon_range)
+
         primer_pair["left_cross_junction"] = is_crossing_left
         primer_pair["right_cross_junction"] = is_crossing_right
-        
+
     return validated_primers
 
 
@@ -35,10 +30,10 @@ def add_intron_len(
 ) -> list[dict]:
     # The additional length does not account for the size of the exons housing the primer pairs."
     added_len = candidate_primers.copy()
-    for primer_pair in added_len :
+    for primer_pair in added_len:
         intron_len = (
-            transcript_record.exon_start_list[primer_pair["right_exon_num"]]
-            - transcript_record.exon_end_list[primer_pair["left_exon_num"]]
+            transcript_record.exon_start_list[primer_pair["right_exon_num"] - 1]
+            - transcript_record.exon_end_list[primer_pair["left_exon_num"] - 1]
             + 1
         )
         if intron_len > 0:
