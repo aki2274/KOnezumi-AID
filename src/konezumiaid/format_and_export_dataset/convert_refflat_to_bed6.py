5dfaed3c6e7e8ea66f551fa361c863cac55addc3
aki2274 <s2110307@s.tsukuba.ac.jp>
2024-06-21 16:15:22 +0900

    タイポスを修正しました．

--- src/konezumiaid/format_and_export_dataset/convert_refflat_to_bed6.py
+++ src/konezumiaid/format_and_export_dataset/convert_refflat_to_bed6.py
@@ -2,7 +2,8 @@ from __future__ import annotations
 import pandas as pd
 from pathlib import Path
 
-def convert_refFlat_to_bed6(df_refflat: pd.DataFrame , output_file: Path) -> None
+
+def convert_refFlat_to_bed6(df_refflat: pd.DataFrame, output_file: Path) -> None:
     with open(output_file, "w") as outfile:
         for _, row in df_refflat.iterrows():
             chrom = row["chrom"]
@@ -12,8 +13,5 @@ def convert_refFlat_to_bed6(df_refflat: pd.DataFrame , output_file: Path) -> Non
             score = "0"
             strand = row["strand"]
 
-            bed_line = "\t".join(
-                [chrom, txStart, txEnd, transcriot_name, score, strand]
-            )
+            bed_line = "\t".join([chrom, txStart, txEnd, transcriot_name, score, strand])
             outfile.write(bed_line + "\n")
-                        
