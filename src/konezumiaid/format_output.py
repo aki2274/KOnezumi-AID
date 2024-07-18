502cd2d3d62d38d708a3569cd042c5b8ae8dd9f1
aki2274 <s2110307@s.tsukuba.ac.jp>
2024-07-18 19:54:05 +0900

    update adding DATAREADME
    DATAREADMEの追加にpyproject.tomlの変更が不要であったため削除しました・
    konezumiaidのバージョンを上げました．
    mkdirの位置が不正確だったので修正しました．

--- src/konezumiaid/format_output.py
+++ src/konezumiaid/format_output.py
@@ -90,13 +90,13 @@ def export_csv(
     df_donor_cand: pd.DataFrame,
 ) -> None:
     output_folder = Path("data", "output")
+    output_folder.mkdir(parents=True, exist_ok=True)
     readme_src = "DATAREADME.md"
     readme_dst = output_folder / "DATAREADME.md"
     with open(readme_src, "r") as f:
         readme_text = f.read()
     with open(readme_dst, "w") as f:
         f.write(readme_text)
-    output_folder.mkdir(parents=True, exist_ok=True)
     df_splice_gRNA = pd.concat([df_acceptor_cand, df_donor_cand])
     if not df_ptc_gRNA.empty:
         df_ptc_gRNA.to_csv(output_folder / f"{name}_ptc_gRNA.csv", index=False)
