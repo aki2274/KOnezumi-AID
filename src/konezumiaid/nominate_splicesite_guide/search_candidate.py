b91be9a4f2053a468f6fc1407b9862d8109e73cc
Taki Taito <140215747+aki2274@users.noreply.github.com>
2024-10-10 01:14:53 +0900

    Update search_candidate.py

--- src/konezumiaid/nominate_splicesite_guide/search_candidate.py
+++ src/konezumiaid/nominate_splicesite_guide/search_candidate.py
@@ -30,6 +30,7 @@ def find_splice_site_candidate(
 
 def filter_candidate(
     candidates: list[dict[int, str]],
+    index_exon_with_5_utr: int,
     index_exon_with_3_utr: int,
     start_pos: list[int],
     end_pos: list[int],
@@ -40,16 +41,17 @@ def filter_candidate(
         grna = cand["seq"][:20]
         target_exon_idx = cand["exon_index"] - 1
         exon_length = end_pos[target_exon_idx] - start_pos[target_exon_idx]
-
         has_no_tttt = "TTTT" not in grna
         is_not_multiple_of_3 = exon_length % 3 != 0
+        is_not_5UTR = target_exon_idx >= index_exon_with_5_utr
+
         if is_acceptor:
             exon_skip_boundary = index_exon_with_3_utr - 2
         else:
             exon_skip_boundary = index_exon_with_3_utr - 3
         is_skiping_induces_nmd = target_exon_idx <= exon_skip_boundary
 
-        if has_no_tttt and is_not_multiple_of_3 and is_skiping_induces_nmd:
+        if has_no_tttt and is_not_multiple_of_3 and is_skiping_induces_nmd and is_not_5UTR:
             filtered_candidates.append(cand)
     return filtered_candidates
 
@@ -70,14 +72,22 @@ def search_site_candidate(
             for i, (start, end) in enumerate(zip(exon_start_pos, exon_end_pos))
             if start < transcript_record.cds_end <= end
         )
-    )
+    ) 
+    index_exon_with_5_utr = next(
+        (
+            i 
+            for i , (start, end) in enumerate(zip(exon_start_pos, exon_end_pos))
+            if start < transcript_record.cds_start <= end
+        )
+    ) 
+
 
     # 22 or 21 means 'G' in AG or GT is in edge of target window
     acceptor_cands = find_splice_site_candidate(orf, exon_start_pos[1:], 22, "AG", 2)
     donor_cands = find_splice_site_candidate(orf, exon_end_pos[:-1], 21, "GT", 1, False)
 
-    acceptor_candidates = filter_candidate(acceptor_cands, index_exon_with_3_utr, exon_start_pos, exon_end_pos)
-    donor_candidates = filter_candidate(donor_cands, index_exon_with_3_utr, exon_start_pos, exon_end_pos)
+    acceptor_candidates = filter_candidate(acceptor_cands,index_exon_with_5_utr, index_exon_with_3_utr, exon_start_pos, exon_end_pos)
+    donor_candidates = filter_candidate(donor_cands,index_exon_with_5_utr, index_exon_with_3_utr, exon_start_pos, exon_end_pos)
 
     acceptor_candidates = link_to_crisperdirect(acceptor_candidates)
     donor_candidates = link_to_crisperdirect(donor_candidates)
